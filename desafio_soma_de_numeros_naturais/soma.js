c17581e4950128c119ff0a47d0189c2d54e80f12
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-06-22 19:46:01 -0300

    feat: :sparkles: Adiciona o desafio SOMA DE NÚMEROS NATURAIS.

---
+++ desafio_soma_de_numeros_naturais/soma.js
@@ -0,0 +1,11 @@
+const A = parseInt(gets());
+const B = parseInt(gets());
+const total = (A + B) * B / 2;
+print(total);
+
+//Sim, é simples assim!
+
+
+//No matter how your heart is grieving
+//if you keep on believing
+//the dream that you wish will come true.
