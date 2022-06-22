f70922a1b00f485ebf28dc88994e6e287a29b5ef
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-06-22 19:52:51 -0300

    feat: :sparkles: Adiciona o desafio ARRAYS PARES.

---
+++ desafio_arrays_pares/arrays.js
@@ -0,0 +1,13 @@
+var array = [ 2, 3, 5, 7, 11, 13, 18, 34 ];
+ 
+for (let i = 0; i < array.length; i++){
+    if (array[i] % 2 == 0){
+        print(array[i]);
+     }          
+}
+
+
+
+//No matter how your heart is grieving
+//if you keep on believing
+//the dream that you wish will come true.
